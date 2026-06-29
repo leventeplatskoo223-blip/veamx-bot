612d93172ef92e6e69d88c2ef5b8c9c331b73cde
leventeplatskoo223-blip <leventeplatskoo223@gmail.com>
2026-06-29 14:26:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => res.send("Bot is alive"));
-app.listen(3000);
-
-require("dotenv").config();
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
+
+// Welcome csatorna ID
+const WELCOME_CHANNEL_ID = "1520774673212903424";
+
+// Imgur kép link
+const IMGUR_LINK = "https://i.imgur.com/ddZEOQ8.png";
 
 client.on("ready", () => {
     console.log(`Bejelentkezve: ${client.user.tag}`);
 });
 
-// IDE ÍRD BE A CSATORNA ID-T
-const WELCOME_CHANNEL_ID = "1520774673212903424";
-
-// IDE ÍRD BE AZ IMGUR LINKET
-const IMGUR_LINK = "https://i.imgur.com/ddZEOQ8.png";
-
-client.on("guildMemberAdd", (member) => {
+// Új tag érkezik
+client.on("guildMemberAdd", async (member) => {
     const channel = member.guild.channels.cache.get(WELCOME_CHANNEL_ID);
     if (!channel) return;
 
-    const embed = new EmbedBuilder()
-        .setTitle(`Szia ${member.user.username}!`)
-        .setDescription("Üdvözlünk a **Veamx PvP Discord Szerverén!**")
-        .setColor("Green")
-        .setImage(IMGUR_LINK);
-
-    channel.send({ embeds: [embed] });
+    channel.send({
+        content: `<@${member.id}> csatlakozott a szerverhez! 👋`,
+        files: [IMGUR_LINK]
+    });
 });
 
 client.login(process.env.TOKEN);
